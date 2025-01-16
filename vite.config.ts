e33e2f74516eebb2f48b4d86f2ffd846a3c268df
akshayphuatne18 <akshayphutane8@gmail.com>
2025-01-16 08:25:20 +0530

    fewchange

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Website",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: "/Portfolio-Website",
 });
